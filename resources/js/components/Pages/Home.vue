423bf35029563175bf9db7331f24569e2a4bb2b1
MaLeVoLenT1 <dionemrobertson@gmail.com>
2023-01-27 16:53:30 -0500

    Added the modal.vue

--- resources/js/components/Pages/Home.vue
+++ resources/js/components/Pages/Home.vue
@@ -2,15 +2,26 @@
     <section class="body-content" id="loading">
         <loading v-if="isLoading === true"/>
         <overlord-parallax-v1 :display="'main'"/>
+
+        <modal v-if="modal" @close="modal = false" :width="'50%'">
+            <h3 slot="header" style="margin-bottom: 0; color: rgb(214, 214, 214);">
+                Welcome !
+            </h3>
+        </modal>
+        <!-- Page Content -->
+        <section class="body-content">
+
+        </section>
     </section>
 </template>
 
 <script>
 import Loading from "../hud/loading.vue";
 import OverlordParallaxV1 from "../hud/parallax/OverlordParallaxV1.vue";
+import Modal from "../hud/modal.vue";
     export default {
         components: {
-            Loading, OverlordParallaxV1
+            Loading, OverlordParallaxV1, Modal
         },
         name: "Home",
         data(){
---
+++ resources/js/components/hud/modal.vue
@@ -0,0 +1,48 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask" :style="'z-index:' + zIndex + ';'">
+            <div class="modal-wrapper">
+                <div class="modal-container OverLord-Shadow-Main" :style="'width:' + width + ';'">
+
+                    <div class="modal-header">
+                        <slot name="header">
+
+                        </slot>
+                    </div>
+
+                    <div class="modal-body">
+                        <slot name="body">
+
+                        </slot>
+                    </div>
+
+                    <div class="modal-footer">
+                        <slot name="footer">
+                            <button class="btn btn-small btn-dark-solid full-width" @click="$emit('close')">
+                                CLOSE
+                            </button>
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: "modal",
+    props:{
+        width:{
+            'default': '80%',
+        },
+        info:{
+            'default':null,
+        },
+        zIndex:{
+            'default': '9998',
+        }
+    }
+}
+</script>
+
